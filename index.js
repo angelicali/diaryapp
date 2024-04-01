fffd2649f7747bbde4143e8bb96c6cc01508ce60
Angelica Li <liyunshuangelica@gmail.com>
2024-04-01 14:31:37 -0500

    toy setup for saving entry and loading saved entry on startup

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const {readFile, readFileSync} = require('fs').promises;
+// const firebase = require('firebase');
+// const firebaseConfig = require('./config');
+
+global.tempDB = {};
+
+// firebase.initializeApp(firebaseConfig);
 
 const app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/', async (request, response)=>{
-    response.send(await readFile('./index.html', 'utf-8'));
+
+app.get('/entry', async (req, res)=>{
+    console.log(global.tempDB);
+    res.status(200).json({entryContent: global.tempDB["testuser"]});
 });
+
+app.post('/entry', async (req, res) => {
+    global.tempDB["testuser"] = req.body;
+    res.status(200).send("received");
+})
 
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`) );
--- public/editor.js
+++ public/editor.js
@@ -6,8 +6,33 @@ const quill = new Quill('#editor', {
       ],
     },
     placeholder: 'Compose an epic...',
-    theme: 'snow', // or 'bubble'
+    theme: 'snow', // or 'bubble',
   });
+
+
+const entryUrl = 'http://localhost:3000/entry';
+fetch(entryUrl, {method: 'GET'}).then((res)=>res.json()).then((jsonData) => {
+  console.log('jsonData');
+  console.log(jsonData);
+  quill.setContents(jsonData.entryContent);
+})
+
+// async function getUserContent() {
+//   const res = await fetch(baseUrl, {
+//     method: 'GET'
+//   })
+//   console.log(res);
+//   return res.json();
+// }
+
+
+// getUserContent().then((userData) => {
+//   console.log("promise fulfilled. user data:");
+//   console.log(userData);
+//   quill.setContents(userData.content);
+  
+// });
+
 
 
 quill.on('text-change', (delta, oldDelta, source) =>{
@@ -23,7 +48,16 @@ quill.keyboard.addBinding({
   key: 's',
   ctrlKey: true,
   handler: function(range, context) {
-    console.log(quill.getContents());
-    // saveEntry("testuser", 123, quill.getContents());
+    const content = quill.getContents();
+    fetch(entryUrl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(content)
+    }).then(() => {
+      console.log('user content saved.');
+    })
+    console.log("key binding exit.");
   }
 });
